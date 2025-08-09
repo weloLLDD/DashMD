554c9158f7c40092345217bdf3cca69ab4671431
john <johnpesene1992@gmail.com>
2025-08-09 11:08:30 +0100

    mise en jour App redux et arrange login

--- src/Redux/Action/userActions.js
+++ src/Redux/Action/userActions.js
@@ -29,7 +29,7 @@ export const login = (email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      `api/users/login`,
+      `https://backenmd.onrender.comapi/api/users/login`,
       { email, password },
       config
     );
